ae9b5cfede891c87bd32afdba7b9edbd1968e975
Vlad Pop <61190539+NoAnswerss@users.noreply.github.com>
2022-05-23 21:46:33 +0100

    added the right images on cards

--- back/index.js
+++ back/index.js
@@ -1,18 +1,16 @@
 
 // Display Dynamic Cards
-var titlearray =["kanap","js","python","java","android","jquery","ruby"];
-var descriptionarray =["css style","js program","python code","java objects","android program","jquery objects","ruby code"];
-var imageArray = ["kanap01.jpeg","kanap02.jpeg","kanap03.jpeg","kanap04.jpeg","kanap05.jpeg","kanap06.jpeg","kanap07.jpeg","kanap08.jpeg"]
+var titlearray =["kanap","kanap","kanap","kanap","kanap","kanap","kanap"];
+var descriptionarray =["kanap","kanap","kanap","kanap","kanap","kanap","kanap"];
+var imageArray = ["kanap01","kanap02","kanap03","kanap04","kanap05","kanap06","kanap07","kanap08"]
 var dynamic = document.querySelector('.items');  
 for (var i = 0; i < titlearray.length; i++) {
   var fetch = document.querySelector('.items').innerHTML;  
   dynamic.innerHTML = `<a href="./product.html?id=42">
   <article>
-  <img src="imageArray[i]" alt="Sofa">
+  <img src="/back/images/${imageArray[i]}.jpeg" alt="Sofa"> 
   <h3 class="productName">${titlearray[i]}</h3>
   <p class="productDescription">${descriptionarray[i]}</p>
 </article>
-</a>` + fetch ; 
-   // var bgimg = document.getElementById(`cards${i}`);
-   // bgimg.style.backgroundImage = `url('img/${titlearray[i]}.jpg')`;
-} 
+</a>` + fetch ; }
+// End of Dynamic Cards Display
