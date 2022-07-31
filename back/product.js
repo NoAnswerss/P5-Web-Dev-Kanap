53115175527aba57bd33f3039fe21fbb783c7779
Vlad Pop <61190539+NoAnswerss@users.noreply.github.com>
2022-07-31 20:05:20 +0100

    inserted single products on product page

--- back/product.js
+++ back/product.js
@@ -1,25 +1,29 @@
 
 // get the id from uRL 
-let searchparams =  new URLSearchParams(window.location.search)
+let searchparams =  new URLSearchParams(window.location.search) 
 let id = searchparams.get('id') 
 console.log('id',id)
+
 function fetchData() {
+  
+  // Inserting a single product and its details into the product page
+
     fetch('http://localhost:3000/api/products/' + id) 
     .then(response => {
         if (!response.ok) {
             throw Error('Error')
         }
         return response.json();
-    }).then(data => {
+    }).then(data => { 
       console.log(data)
         document.getElementById('title').innerText = data.name
         document.getElementById('price').innerText = data.price
-        document.getElementById('description').innerText = data.description
-        document.getElementById('sofa_image').src = data.imageUrl
+        document.getElementById('description').innerText = data.description 
+        document.getElementById('sofa_image').src = data.imageUrl // Images need src instead of innerText or innerHTML
         // Created map to display array of colors 
         let colors = document.getElementById('colors')
         data.colors.map(color=>{
-          colors.insertAdjacentHTML('beforeend', `<option value=${color}>${color}</option>`)
+          colors.insertAdjacentHTML('beforeend', `<option value=${color}>${color}</option>`) 
         })
         // document.getElementById('title').innerText = data.name
         // document.getElementById('title').innerText = data.name
@@ -30,10 +34,8 @@ function fetchData() {
 
 fetchData();
 
-/* <a href="./product.html?${sofa._id}">
-<article>
-<img src="${sofa.imageUrl}" alt=${sofa.altTxt}>
-<h3 class="productName">${sofa.name}</h3>
-<p class="productDescription">${sofa.description}</p>
-</article>
-</a> */
+// Add to Cart
+
+function addToCart(id) {
+  console.log(id);
+}
