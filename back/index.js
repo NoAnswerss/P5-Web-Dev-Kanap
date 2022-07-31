6cf4e9c654832350e44145499a86dab7ee3e16d9
Vlad Pop <61190539+NoAnswerss@users.noreply.github.com>
2022-07-31 20:02:57 +0100

    inserting single products on product page

--- back/index.js
+++ back/index.js
@@ -41,7 +41,7 @@ for (var i = 0; i < titlearray.length; i++) {
       console.log(data); 
       const html = data.map(sofa => {
         return `
-          <a href="./product.html?${sofa._id}">
+          <a href="./product.html?id=${sofa._id}">
           <article>
           <img src="${sofa.imageUrl}" alt=${sofa.altTxt}>
           <h3 class="productName">${sofa.name}</h3>
--- back/product.js
+++ back/product.js
@@ -1,31 +1,28 @@
+
+// get the id from uRL 
+let searchparams =  new URLSearchParams(window.location.search)
+let id = searchparams.get('id') 
+console.log('id',id)
 function fetchData() {
-    fetch('http://localhost:3000/api/products')
+    fetch('http://localhost:3000/api/products/' + id) 
     .then(response => {
         if (!response.ok) {
             throw Error('Error')
         }
         return response.json();
     }).then(data => {
-        console.log(data); 
-        const html = data.map(sofa => {
-            return `
-            <div class="item__img">
-            <img src="${sofa.imageUrl}" alt=${sofa.altTxt}>
-            </div>
-            <div class="item__content">
-
-              <div class="item__content__titlePrice">
-                <h1 id="title">${sofa.name}</h1>
-                <p>Prix : <span id="price">${sofa.price}</span>€</p>
-              </div>
-
-              <div class="item__content__description">
-                <p class="item__content__description__title">${sofa.description}:</p>
-              </div>
-            `; 
-        }).join("");
-        console.log(html);
-        document.querySelector('.item').insertAdjacentHTML("afterbegin", html) // Used after begin and got right result
+      console.log(data)
+        document.getElementById('title').innerText = data.name
+        document.getElementById('price').innerText = data.price
+        document.getElementById('description').innerText = data.description
+        document.getElementById('sofa_image').src = data.imageUrl
+        // Created map to display array of colors 
+        let colors = document.getElementById('colors')
+        data.colors.map(color=>{
+          colors.insertAdjacentHTML('beforeend', `<option value=${color}>${color}</option>`)
+        })
+        // document.getElementById('title').innerText = data.name
+        // document.getElementById('title').innerText = data.name
     }).catch(error => {
         console.log(error);
     });
